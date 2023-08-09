a9df387096881faa2a28dc9e41dbf4fe9f28e7a5
Ollan Muza <128623143+kxngollan@users.noreply.github.com>
2023-08-09 11:59:54 +0100

    my-app change to frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import ListResults from './components/ListResults';
+import UserInput from './components/UserInput';
+import './App.css';
+
+const taskArray = [];
+
+const App = () => {
+  // This is not working
+  //   const sortedTasksArray = [...taskArray].sort(
+  //     (taskA, taskB) => new Date(taskA.date) - new Date(taskB.date)
+  //   );
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch('/api')
+      .then((response) => response.json())
+      .then((data) => setBackendData(data));
+  }, []);
+
+  const [tasks, setTasks] = useState(taskArray);
+
+  const onSubmitHandler = (task) => {
+    setTasks((prevTasks) => {
+      return [task, ...prevTasks];
+    });
+  };
+
+  const onDeleteItemHandler = (taskId) => {
+    setTasks((prevTasks) => {
+      let updateTasks = prevTasks.filter((task) => task.id !== taskId);
+      return updateTasks;
+    });
+  };
+
+  return (
+    <div className="">
+      <UserInput onAddTask={onSubmitHandler} />
+      <ListResults tasks={tasks} onDeleteItem={onDeleteItemHandler} />
+    </div>
+  );
+};
+
+export default App;
