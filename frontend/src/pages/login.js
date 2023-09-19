065aa3f31a9d6ecb386736c1d382fb433d8a6f95
Ollan Muza <128623143+kxngollan@users.noreply.github.com>
2023-09-19 14:04:20 +0100

    add login and register page

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,41 @@
+const login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <form>
+      <label for="email">email</label>
+      <input
+        type="email"
+        name="email"
+        placeholder="example@email.com"
+        id="email"
+        required
+        onChange={emailChangeHandler}
+        value={email}
+      />
+      <label for="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        placeholder="password"
+        id="password"
+        required
+        onChange={passwordChangeHandler}
+        value={password}
+      />
+      <button>Login</button>
+      <button>Don't have an account</button>
+    </form>
+  );
+};
+
+export default login;
